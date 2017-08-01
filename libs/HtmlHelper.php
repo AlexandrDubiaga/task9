14bf72036bc627c58e27c9579404feb941f719fa
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 19:41:52 +0300

    Update HtmlHelper.php

--- libs/HtmlHelper.php
+++ libs/HtmlHelper.php
@@ -33,7 +33,7 @@ abstract class HtmlHelper{
 			$class = (isset($class) && !empty($class)) ? ' class="' . $class . '"': null;
 			$string = '';
 			$i = 0;
-			$string .= "<select $class style="size:$x;">";
+			$string .= "<select $class size:$x">";
 			foreach ($select as $key => $val) {
 				$i++;
 				$string  .= '<option id="' . $i . '"' . $class . '>' . PHP_EOL . $val . PHP_EOL . '</option>' . PHP_EOL;	
