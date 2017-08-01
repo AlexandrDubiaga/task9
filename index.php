020cc9d438831c818266db8a0331dca8d0958c43
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 18:06:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 include_once(lids/HtmlHelper.php');
 echo HTML::Doctype();
-$fields = array('text' => array('id' => 'username', 'name' => 'username', 'placeholder' =>  'text input!'),
-'password' => array('id' => 'password', 'name' => 'password', 'placeholder' => 'password input!'));
+
 echo HTML::Form('index.php', $fields);
 ?>
