9c023ce4beea1589539f635753667904e24cff65
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 19:45:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ include_once('libs/HtmlHelper.php');
 			   );
 
 $form =  HtmlHelper::Form('template/index.php', $fields);
-$select = array('option' => 'first', 'id' => 'second', 'name' => 'third');
 */
+$select = array('option' => 'first', 'id' => 'second', 'name' => 'third');
+
 $selectMult = HtmlHelper::select($select,'liClass',20);
 include_once('template/index.php');
 ?>
