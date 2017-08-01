f1099e43fef52f1fcd1c8bc8fa8b3db302a0d9cd
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 20:55:51 +0300

    Update HtmlHelper.php

--- libs/HtmlHelper.php
+++ libs/HtmlHelper.php
@@ -66,16 +66,6 @@ abstract class HtmlHelper{
 	}
 	
 	
-	public static function Form($action, $fields, $name = null, $method = 'post', $enctype = 'multipart/form-data') {
-		$name = (isset($name) && !empty($name)) ? ' name="' . $name . '"' : null;
-		$method = (isset($method)) ? ' method="' . $method . '"': null;
-		$enctype = (isset($enctype)) ? ' enctype="' . $enctype . '"': null;
-		$html = '<form action="' . $action . '"' . $name . $method . $enctype . '>' . PHP_EOL;
-		$html .= self::parse_fields($fields);
-		$html .= '</form>' . PHP_EOL;
-		return $html;
-	}
-	
 	
 }
 ?>
