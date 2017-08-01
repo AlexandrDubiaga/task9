93dafab0a8b4abd10254da1d5c5206883da96f85
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 18:19:37 +0300

    Update index.php

--- template/index.php
+++ template/index.php
@@ -14,7 +14,11 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   </head>
   <body>
-    <h1>Hello world</h1>
+    <?php
+      echo $form;
+    
+    
+    ?>
 
    
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
