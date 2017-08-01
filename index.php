d8e201a82fe586f0aaf2944ab5573cd9209bfeb9
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 18:19:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ $fields = array(
 				'text' => array('id' => 'username', 'name' => 'username', 'placeholder' => 'I\'m a text input!'),
 				'password' => array('id' => 'password', 'name' => 'password', 'placeholder' => 'I\'m a password input!')
 			   );
-echo HtmlHelper::Form('template/index.php', $fields);
+$form =  HtmlHelper::Form('template/index.php', $fields);
+include_once('template/index.php');
 ?>
