5ed2c49b7b441ee72c2294f95b306a00180c699a
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 18:26:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include_once('libs/HtmlHelper.php');
-echo HtmlHelper::Doctype();
 $fields = array(
 				'text' => array('id' => 'username', 'name' => 'username', 'placeholder' => 'I\'m a text input!'),
 				'password' => array('id' => 'password', 'name' => 'password', 'placeholder' => 'I\'m a password input!')
