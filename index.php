71b5061ba0a01c77725f97610e8a79bfb073c51c
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 18:40:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $fields = array(
 				'text' => array('id' => 'username', 'name' => 'username', 'placeholder' => 'I\'m a text input!'),
 				'password' => array('id' => 'password', 'name' => 'password', 'placeholder' => 'I\'m a password input!')
 			   );
+
 $form =  HtmlHelper::Form('template/index.php', $fields);
+$list = array('li' => array('id' => 'alex', 'name' => 'test', 'value' => 'hello'));
+
+$list = HtmlHelper::list_item($list,'class');
 include_once('template/index.php');
 ?>
