f2087ff657b2d48e10cc7da57ca54da85c9a7a48
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-01 19:44:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 include_once('libs/HtmlHelper.php');
-$fields = array(
+/*$fields = array(
 				'text' => array('id' => 'username', 'name' => 'username', 'placeholder' => 'I\'m a text input!'),
 				'password' => array('id' => 'password', 'name' => 'password', 'placeholder' => 'I\'m a password input!')
 			   );
 
 $form =  HtmlHelper::Form('template/index.php', $fields);
 $select = array('option' => 'first', 'id' => 'second', 'name' => 'third');
-
-echo $list = HtmlHelper::select($select,'liClass',20);
+*/
+$selectMult = HtmlHelper::select($select,'liClass',20);
 include_once('template/index.php');
 ?>
