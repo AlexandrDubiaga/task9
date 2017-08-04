0765b1dabf2303843bb6a17ea6a0b9557d0999b1
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-04 18:11:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,19 +11,34 @@ if(!$selectMult)
 $titles = array('Name', 'Title', 'Date');
 $tab = array('Alex', 'Story', '12.02.17');
 $table = HtmlHelper::table($tab, $titles, 'table table-striped');
-
+if(!$table)
+{
+  $errorTable = ERROR_TABLE;
+}
 $oLUlData = array('first', 'Second', 'Fight');
 $listUlOl = HtmlHelper::listesOlUl($oLUlData, 'list-group','list-group-item','ol');
-
+if(!$listUlOl)
+{
+  $errorListOlUl = ERROR_LISTOLUL;
+}
 $dtData = array('varOne'=>'first', 'varTwo'=>'Second', 'varThree'=>'Fight');
 $dtddlList = HtmlHelper::listesdlDtDd($dtData,'list-group','list-group-item');
-
+if(!$dtddlList)
+{
+  $errorListDtDdDl = ERROR_LISTDTDDDL;
+}
 $valradio = array('varOne'=>'first', 'varTwo'=>'Second', 'varThree'=>'Fight');
 $radioForm = HtmlHelper::radiobuttonsGroup($valradio,'form-inline','','group');
-
+if(!$radioForm)
+{
+  $errorRadioForm = ERROR_RADIOFORM;
+}
 $valCheckbox = array('varOne'=>'first', 'varTwo'=>'Second', 'varThree'=>'Fight');
 $checkboxForm = HtmlHelper::checkbox($valCheckbox,'form-inline','','val','varTwo');
-
+if(!$checkboxForm)
+{
+  $errorCheckboxForm = ERROR_CHECKBOXFORM;
+}
 include_once('template/index.php');
 
 ?>
