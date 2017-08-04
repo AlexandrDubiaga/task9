4a573dd9cf22486d5dbe9ebe3ee365f45591fb43
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-04 16:56:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ $titles = array('Name', 'Title', 'Date');
 $tab = array('Alex', 'Story', '12.02.17');
 $table = HtmlHelper::table($tab, $titles, 'table table-striped');
 
-$tab = array('first', 'Second', 'Fight');
-$listUl = HtmlHelper::listesOlUl($tab, 'list-group','list-group-item','ol');
+$oLUlData = array('first', 'Second', 'Fight');
+$listUlOl = HtmlHelper::listesOlUl($oLUlData, 'list-group','list-group-item','ol');
 
-$val = array('varOne'=>'first', 'varTwo'=>'Second', 'varThree'=>'Fight');
-$dtdddlList = HtmlHelper::listesdlDtDd($val,'list-group','list-group-item');
+$dtData = array('varOne'=>'first', 'varTwo'=>'Second', 'varThree'=>'Fight');
+$dtddlList = HtmlHelper::listesdlDtDd($dtData,'list-group','list-group-item');
 
 $valradio = array('varOne'=>'first', 'varTwo'=>'Second', 'varThree'=>'Fight');
 $radioForm = HtmlHelper::radiobuttonsGroup($valradio,'form-inline','','group');
