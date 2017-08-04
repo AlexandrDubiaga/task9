f3775fffd3fa0aab72e5fc44590f27aef2a1e820
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-04 18:07:01 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 <?php
 define('ERROR_SELECT','Error select Form');
-
+define('ERROR_TABLE','Error table');
+define('ERROR_LISTOLUL','Error list ol ul');
+define('ERROR_LISTDTDDDL','Error list dl dt dd');
+define('ERROR_RADIOFORM','Error radio Form');
+define('ERROR_CHECKBOXFORM','Error checkbox Form');
 ?>
